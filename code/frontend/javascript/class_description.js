0d1c8401513bdc75cefe43b27b94fe3b7f63335d
MoGHenry <moghenry0@gmail.com>
2022-10-16 18:36:47 -0400

    class result/description page

---
+++ code/frontend/javascript/class_description.js
@@ -0,0 +1,5 @@
+function show_description(class_json)
+{
+
+
+}
---
+++ code/frontend/javascript/search_result.js
@@ -0,0 +1,15 @@
+
+const obj = document.getElementById('class-div');
+
+obj.innerHTML += '' +
+    '    <div class="container px-4 my-4 text-center">\n' +
+    '        <div class="row gx-5">\n' +
+    '            <div class="col">\n' +
+    '                <div class="p-2 border bg-light">Custom column padding</div>\n' +
+    '            </div>\n' +
+    '            <div class="col">\n' +
+    '                <div class="p-2 border bg-light">Custom column padding</div>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '    </div>' +
+    '';
