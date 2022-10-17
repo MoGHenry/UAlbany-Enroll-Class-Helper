f1dbe6415ecb6577d88727ba02bb1965f572b40c
YunRuiHuang <yunruihuang@gmail.com>
2022-10-17 15:01:40 -0400

    finish json format

--- code/middleware/WebAPI/Controllers/TodoItemsController.cs
+++ code/middleware/WebAPI/Controllers/TodoItemsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebAPI.Models;
+using Newtonsoft.Json;
 
 namespace WebAPI.Controllers
 {
@@ -22,9 +23,57 @@ namespace WebAPI.Controllers
 
         // GET: api/TodoItems
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        public async Task<ActionResult<TodoItem>> GetTodoItems()
         {
-            return await _context.TodoItems.ToListAsync();
+            ClassDataType classDataType1 = new ClassDataType()
+            {
+                level = "Undergraduate",
+                department = "Africana Studies",
+                classNumber = 6882,
+                courseInfo = "AAFS 101 Intro to Africana Studies",
+                meetingTime = "TTH 04:30_PM-05:50_PM",
+                meetingLocation = "Humanities 024 Agum,David",
+                prerequest = "",
+                labrequest = "",
+                creditRange = "3.0- 3.0",
+                classType = "",
+                seatsRemain = 0,
+                session = "FULL",
+                courseDelivery = "INPERSON",
+                genEd = "CHALLENGES",
+                specialRestriction = "None",
+                description = "This course will introduce students to the historical foundations of Africana Studies and discuss its relevance to contemporary society. An interdisciplinary approach will be incorporated as History, Philosophy, Literature, Performing Arts, Sociology, Psychology, Religion/Spirituality, and Anthropology are employed to provide students a detailed analysis of people of African descent in the Western Hemisphere. ",
+                other = ""
+            };
+            ClassDataType classDataType2 = new ClassDataType()
+            {
+                level = "Undergraduate",
+                department = "Africana Studies",
+                classNumber = 9752,
+                courseInfo = "AAFS 150 Life In The Third World",
+                meetingTime = "12:00_AM-12:00_AM",
+                meetingLocation = "Online Agum,David",
+                prerequest = "",
+                labrequest = "",
+                creditRange = "3.0- 3.0",
+                classType = "",
+                seatsRemain = 0,
+                session = "8W2",
+                courseDelivery = "ONLASYNCH/ONLFULL",
+                genEd = "INTLPER",
+                specialRestriction = "None",
+                description = "Introduction to cultural variation and fragmentation among third-world developing communities. Some lectures and discussions are led by third-world graduate students. Whenever possible, distinguished visitors from third-world countries are also involved in the course.",
+                other = ""
+            };
+            ClassDataType[] classDataTypes = new ClassDataType[2];
+            classDataTypes[0] = classDataType1;
+            classDataTypes[1] = classDataType2;
+            TodoItem todoItem = new TodoItem() {
+                Id=0,
+                number = 2,
+                value = JsonConvert.SerializeObject(classDataTypes)
+            };
+            return todoItem;
         }
 
         // GET: api/TodoItems/5
--- code/middleware/WebAPI/Models/TodoItem.cs
+++ code/middleware/WebAPI/Models/TodoItem.cs
@@ -3,22 +3,7 @@
     public class TodoItem 
     {
         public long Id { get; set; }
-        public string level { get; set; } //3
-        public string department { get; set; } //4
-        public int classNumber { get; set; } //5
-        public string courseInfo { get; set; } //6
-        public string meetingTime { get; set; } //7
-        public string meetingLocation { get; set; }
-        public string prerequest { get; set; } //8
-        public string labrequest { get; set; }
-        public string creditRange { get; set; } //9
-        public string classType { get; set; } //10
-        public int seatsRemain { get; set; } //11
-        public string session { get; set; } //12
-        public string courseDelivery { get; set; } //13
-        public string genEd { get; set; } //14
-        public string specialRestriction { get; set; } //15
-        public string description { get; set; } //16
-        public string other { get; set; } //17 
+        public int number { get; set; } 
+        public string value { get; set; } 
     }
 }
