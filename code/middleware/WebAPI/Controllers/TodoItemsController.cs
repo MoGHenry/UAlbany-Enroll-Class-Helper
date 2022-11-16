30ce861b67ae7368a48be5fac6190c98061a820e
YunRuiHuang <yunruihuang@gmail.com>
2022-11-15 22:55:21 -0500

    fiinish

--- code/middleware/WebAPI/Controllers/TodoItemsController.cs
+++ code/middleware/WebAPI/Controllers/TodoItemsController.cs
@@ -15,6 +15,7 @@ namespace WebAPI.Controllers
     public class TodoItemsController : ControllerBase
     {
         private readonly TodoContext _context;
+        private Connector.Connector connector = new Connector.Connector();
 
         public TodoItemsController(TodoContext context)
         {
@@ -25,54 +26,15 @@ namespace WebAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<TodoItem>> GetTodoItems()
         {
-            ClassDataType classDataType1 = new ClassDataType()
-            {
-                level = "Undergraduate",
-                department = "Africana Studies",
-                classNumber = 6882,
-                courseInfo = "AAFS 101 Intro to Africana Studies",
-                meetingTime = "TTH 04:30_PM-05:50_PM",
-                meetingLocation = "Humanities 024 Agum,David",
-                prerequest = "",
-                labrequest = "",
-                creditRange = "3.0- 3.0",
-                classType = "",
-                seatsRemain = 0,
-                session = "FULL",
-                courseDelivery = "INPERSON",
-                genEd = "CHALLENGES",
-                specialRestriction = "None",
-                description = "This course will introduce students to the historical foundations of Africana Studies and discuss its relevance to contemporary society. An interdisciplinary approach will be incorporated as History, Philosophy, Literature, Performing Arts, Sociology, Psychology, Religion/Spirituality, and Anthropology are employed to provide students a detailed analysis of people of African descent in the Western Hemisphere. ",
-                other = ""
-            };
-            ClassDataType classDataType2 = new ClassDataType()
-            {
-                level = "Undergraduate",
-                department = "Africana Studies",
-                classNumber = 9752,
-                courseInfo = "AAFS 150 Life In The Third World",
-                meetingTime = "12:00_AM-12:00_AM",
-                meetingLocation = "Online Agum,David",
-                prerequest = "",
-                labrequest = "",
-                creditRange = "3.0- 3.0",
-                classType = "",
-                seatsRemain = 0,
-                session = "8W2",
-                courseDelivery = "ONLASYNCH/ONLFULL",
-                genEd = "INTLPER",
-                specialRestriction = "None",
-                description = "Introduction to cultural variation and fragmentation among third-world developing communities. Some lectures and discussions are led by third-world graduate students. Whenever possible, distinguished visitors from third-world countries are also involved in the course.",
-                other = ""
-            };
-            ClassDataType[] classDataTypes = new ClassDataType[2];
-            classDataTypes[0] = classDataType1;
-            classDataTypes[1] = classDataType2;
+            
+            ClassDataType[] classDataTypes = connector.searchWithTitle("ICSI210");
             TodoItem todoItem = new TodoItem() {
-                Id=0,
+                Id = 0,
                 number = 2,
-                value = JsonConvert.SerializeObject(classDataTypes)
+                value = JsonConvert.SerializeObject("ICSI210")
             };
+            
+
             return todoItem;
         }
 
@@ -126,8 +88,15 @@ namespace WebAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
         {
-            _context.TodoItems.Add(todoItem);
-            await _context.SaveChangesAsync();
+            if(todoItem.number == 1)
+            {
+                todoItem.value = JsonConvert.SerializeObject(connector.searchWithSubject(todoItem.value));
+            }else if(todoItem.number == 2)
+            {
+                todoItem.value = JsonConvert.SerializeObject(connector.searchWithTitle(todoItem.value));
+            }
+            //_context.TodoItems.Add(todoItem);
+            //await _context.SaveChangesAsync();
 
             //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
             return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
